66c64b1b852d1821bdb8858cb4f199462578737e
박예진 <qkrdpwls2002>
2024-11-14 10:53:39 +0900

    start

---
+++ src/main/java/com/example/spartaschedulev2/SpartaScheduleV2Application.java
@@ -0,0 +1,13 @@
+package com.example.spartaschedulev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaScheduleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaScheduleV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spartaschedulev2/SpartaScheduleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartaschedulev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaScheduleV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
